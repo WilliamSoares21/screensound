f6b9e65343859d020b1b83949bf3339672db065b
William Soares <pedro.williamsoares2022@gmail.com>
2026-02-24 14:04:38 -0300

    feat: adicionar query JPQL para buscar músicas por artista

--- src/main/java/com/screensound/screensound/repository/ArtistaRepository.java
+++ src/main/java/com/screensound/screensound/repository/ArtistaRepository.java
@@ -1,12 +1,18 @@
 package com.screensound.screensound.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.screensound.screensound.model.Artista;
+import com.screensound.screensound.model.Musica;
 
 public interface ArtistaRepository extends JpaRepository<Artista, Long> {
 
   Optional<Artista> findByNomeContainingIgnoreCase(String nome);
+
+  @Query("SELECT m FROM Artista a JOIN a.musicas m WHERE LOWER(a.nome) LIKE LOWER(CONCAT('%', :nome, '%'))")
+  List<Musica> buscarMusicasPorArtista(String nome);
 }
